b6a6d412809a67a2450b10d4bdbdd062fddc24ef
Asadi Ceccarelli <asadiceccarelli@Asadis-MBP.lan>
2022-06-29 11:21:01 +0100

    This completes Milestone 1.

--- hangman/hangman_solution.py
+++ hangman/hangman_solution.py
@@ -76,14 +76,23 @@ class Hangman:
         # TODO 1: Ask the user for a letter iteratively until the user enters a valid letter
         # TODO 1: Assign the letter to a variable called `letter`
         # TODO 1: The letter has to comply with the following criteria: It has to be a single character. If it is not, print "Please, enter just one character"
+        valid_letter = False
+        while valid_letter == False:
+            letter = input('Enter a letter and press enter: ')
+            if len(letter) != 1:
+                print('Please, enter just one character')
+            else:
+                valid_letter = True
+    
         # TODO 2. It has to be a letter that has not been tried yet. Use the list_letters attribute to check this. If it has been tried, print "{letter} was already tried".
         # TODO 3: If the letter is valid, call the check_letter method
-        pass
+        return letter
 
 def play_game(word_list):
     # As an aid, part of the code is already provided:
     game = Hangman(word_list, num_lives=5)
     # TODO 1: To test this task, you can call the ask_letter method
+    game.ask_letter()
     # TODO 2: To test this task, upon initialization, two messages should be printed 
     # TODO 3: To test this task, you call the ask_letter method and check if the letter is in the word
     
@@ -96,4 +105,3 @@ def play_game(word_list):
 if __name__ == '__main__':
     word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
     play_game(word_list)
-# %%
